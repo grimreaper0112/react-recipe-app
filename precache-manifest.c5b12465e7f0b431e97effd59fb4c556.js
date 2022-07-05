73d7cfc2f364d1f7dea2d80df32f0aa7b3eff8ad
grimreaper0112 <stefanfer01@gmail.com>
2022-07-05 06:39:21 +0530

    Updates

---
+++ precache-manifest.c5b12465e7f0b431e97effd59fb4c556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d63cd74d6fd0d32b8f5",
+    "url": "/react-recipe-app/static/css/main.a34ad397.chunk.css"
+  },
+  {
+    "revision": "7d63cd74d6fd0d32b8f5",
+    "url": "/react-recipe-app/static/js/main.7d63cd74.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/react-recipe-app/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "af7a7be8828dc3c4c6ad",
+    "url": "/react-recipe-app/static/js/2.af7a7be8.chunk.js"
+  },
+  {
+    "revision": "9d52df1af34cd2c9758b",
+    "url": "/react-recipe-app/static/js/runtime~main.9d52df1a.js"
+  },
+  {
+    "revision": "7426042ac58bd2374660a8a04bab1119",
+    "url": "/react-recipe-app/index.html"
+  }
+];
